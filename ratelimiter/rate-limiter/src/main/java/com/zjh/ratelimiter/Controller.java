ebf6fbe098aca36ff7efb4fafaa4793bcd9d4579
zhaojh0610 <zhaojh0610@163.com>
2020-11-13 21:34:30 +0800

    zjh -d 基于springCloud微服务改造工程开始

---
+++ ratelimiter/rate-limiter/src/main/java/com/zjh/ratelimiter/Controller.java
@@ -0,0 +1,60 @@
+package com.zjh.ratelimiter;
+
+import com.google.common.util.concurrent.RateLimiter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author zhaojh
+ * @date 2020/11/12 20:59
+ */
+@RestController
+@Slf4j
+public class Controller {
+
+    RateLimiter limiter = RateLimiter.create(2);
+
+    //  非阻塞限流
+    @RequestMapping("tryAcquire")
+    public String tryAcquire(Integer count) {
+        if (limiter.tryAcquire(count)) {
+            log.info("success, rate is {}", limiter.getRate());
+            return "success";
+        } else {
+            log.info("fail, rate is {}", limiter.getRate());
+            return "fail";
+        }
+    }
+
+    //  限定时间的非阻塞限流
+    @RequestMapping("tryAcquireWithTimeout")
+    public String tryAcquireWithTimeout(Integer count, Integer timeout) {
+        if (limiter.tryAcquire(count, timeout, TimeUnit.SECONDS)) {
+            log.info("success, rate is {}", limiter.getRate());
+            return "success";
+        } else {
+            log.info("fail, rate is {}", limiter.getRate());
+            return "fail";
+        }
+    }
+
+    //  同步阻塞限流
+    @RequestMapping("acquire")
+    public String acquire(Integer count) {
+        limiter.acquire(count);
+        log.info("success, rate is {}", limiter.getRate());
+        return "success";
+    }
+
+    @RequestMapping("nginx")
+    public String nginx() {
+        log.info("nginx success");
+        return "success";
+    }
+
+
+
+}
---
+++ ratelimiter/rate-limiter/src/main/java/com/zjh/ratelimiter/RateLimiterApplication.java
@@ -0,0 +1,16 @@
+package com.zjh.ratelimiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhaojh
+ */
+@SpringBootApplication
+public class RateLimiterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RateLimiterApplication.class, args);
+    }
+
+}
