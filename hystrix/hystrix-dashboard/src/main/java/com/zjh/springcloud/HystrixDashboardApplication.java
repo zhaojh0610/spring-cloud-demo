64a65c7fea28a91d0ba9eec9f92e8a551872c22e
zhaojh0610 <zhaojh0610@163.com>
2020-11-21 08:54:22 +0800

    zjh -d turbine集成大盘监控-2

---
+++ hystrix/hystrix-dashboard/src/main/java/com/zjh/springcloud/HystrixDashboardApplication.java
@@ -0,0 +1,21 @@
+package com.zjh.springcloud;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author zhaojh
+ * @date 2020/11/21 8:18
+ */
+@EnableHystrixDashboard
+@SpringCloudApplication
+public class HystrixDashboardApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(HystrixDashboardApplication.class)
+                .web(WebApplicationType.SERVLET)
+                .run(args);
+    }
+}
