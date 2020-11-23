539829c3bb89ef03218fb64f8f46f6ea6761a1e5
zhaojh0610 <zhaojh0610@163.com>
2020-11-23 10:11:03 +0800

    zjh -d 【Demo】Gateway极速落地-创建默认路由规则

---
+++ gateway/gateway-sample/src/main/java/com/zjh/springcloud/GatewayApplication.java
@@ -0,0 +1,18 @@
+package com.zjh.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author zhaojh
+ * @date 2020/11/23 9:17
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
